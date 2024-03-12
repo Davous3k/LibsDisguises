cacd5997f4c9c4bfb398e9fa0109e68d76327c47
David Mikluš <42912770+Davous3k@users.noreply.github.com>
2024-03-12 16:48:30 +0100

    Update DisguiseBaseCommand.java

--- plugin/src/main/java/me/libraryaddict/disguise/commands/DisguiseBaseCommand.java
+++ plugin/src/main/java/me/libraryaddict/disguise/commands/DisguiseBaseCommand.java
@@ -90,13 +90,6 @@ public abstract class DisguiseBaseCommand implements CommandExecutor {
         // Already sent the message, assign after the previous
         updatedVersion = true;
 
-        if (sender instanceof Player && !sender.isOp() &&
-            (!LibsPremium.isPremium() || LibsPremium.getPaidInformation() == LibsPremium.getPluginInformation())) {
-            sender.sendMessage(ChatColor.RED + "This is the free version of Lib's Disguises, player commands are limited to console and " +
-                "Operators only! Purchase the plugin for non-admin usage!");
-            return true;
-        }
-
         return false;
     }
 
